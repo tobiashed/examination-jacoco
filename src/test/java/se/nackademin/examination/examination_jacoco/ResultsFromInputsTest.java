82605c2fa8444a7f18ef4777c23ba5aef17fe88a
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-24 11:41:45 +0100

    added all tests in ResultFromInputTest class

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -7,10 +7,27 @@ import org.junit.Test;
 public class ResultsFromInputsTest {
 
 	@Test
-	public void testAgeGettersAndSettersMethods() {
+	public void testNameGettersAndSettersMethods() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
 	}
-
+	@Test
+	public void testAgeGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForAge(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForAge(), 10);
+	}
+	@Test
+	public void testGenderGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForGender(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForGender(), 10);
+	}
+	@Test
+	public void testHomeCityGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForHomeCity(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForHomeCity(), 10);
+	}
 }
