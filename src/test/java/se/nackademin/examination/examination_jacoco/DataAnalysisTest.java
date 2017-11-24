8f59764ab91db43464b2433227d6c91375f3dd8d
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-24 11:10:38 +0100

    added all tests in DataAnalasysTest class

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -19,5 +19,54 @@ public class DataAnalysisTest {
 				result.contains("The name of the homecity is big and the participant is 30 or older"));
 
 	}
+	@Test
+	public void test2() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "20", "Fortaleza"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is big and the participant is younger than 30' ",
+				result.contains("The name of the homecity is big and the participant is younger than 30"));
 
+	}
+	@Test
+	public void test3() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "20", "Fort"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is younger than 30' ",
+				result.contains("The name of the homecity is small and the participant is younger than 30"));
+
+	}
+	@Test
+	public void test4() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fort"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is 30 or older' ",
+				result.contains("The name of the homecity is small and the participant is 30 or older"));
+
+	}
+	@Test
+	public void test5() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "31", "Fort"));
+		String result = dataAnalysis.getResultStringNamesAndAge(values);
+		assertTrue("The result should contain 'The first name is greater or equals in size to the last name and the participant older than 30' ",
+				result.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+		
+	}
+	@Test
+	public void test6() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silvaaaaa", "M", "20", "Fort"));
+		String result = dataAnalysis.getResultStringNamesAndAge(values);
+		assertTrue("The result should contain 'The first name is smaller or equals in size to the last name and the participant is 30 or younger' ",
+				result.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+		
+	}
 }
